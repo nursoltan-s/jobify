c8aaff694cf86664aad9e4db03da8ff08c1f15a4
Nursoltan Saipolda <nursoltan.saipolda@rubycluster.com>
2019-08-15 13:27:23 +0800

    added axios & get post request

--- app/javascript/components/NewPost.js
+++ app/javascript/components/NewPost.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import axios from 'axios';
+import { passCsrfToken } from '../util/helpers';
 
 class NewPost extends React.Component {
+  state = {
+    title: '',
+    body: ''
+  };
+
+  componentDidMount() {
+    passCsrfToken(document, axios);
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    axios.post('/api/posts', post).then(response => {
+      console.log(response);
+      console.log(response.data);
+    });
+  };
+
   render() {
-    return <div>NewPost page</div>;
+    return (
+      <div>
+        <h1>New Post</h1>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input name="title" placeholder="title" type="text" />
+          <input name="body" placeholder="body" type="text" />
+          <button>Create Post</button>
+        </form>
+      </div>
+    );
   }
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
+  namespace :api, defaults: { format: 'json' } do
+    resources :posts, only: [:index, :create]
+  end
+
   match '*path', to: 'pages#index', via: :all
 end
