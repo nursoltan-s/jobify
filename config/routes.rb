218a8c3906706fd02a8ae2d2e423ee14163be847
Nursoltan Saipolda <nursoltan.saipolda@rubycluster.com>
2019-08-19 11:04:15 +0800

    deleted whole repo

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#index'
-
-  namespace :api, defaults: { format: 'json' } do
-    resources :posts, only: [:index, :create]
-  end
-
-  match '*path', to: 'pages#index', via: :all
-end
